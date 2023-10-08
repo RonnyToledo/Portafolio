a09fb7422f681ed384c6259e792f864ac66fd99b
Ronny Toledo <139065928+RonnyToledo@users.noreply.github.com>
2023-10-08 19:25:12 -0400

    Add files via upload

---
+++ correo.js
@@ -0,0 +1,21 @@
+const btn = document.getElementById("enviar-whatsapp");
+
+document.getElementById("form").addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  btn.value = "Sending...";
+
+  const serviceID = "default_service";
+  const templateID = "template_hgv6gkb";
+
+  emailjs.sendForm(serviceID, templateID, this).then(
+    () => {
+      btn.value = "Send Email";
+      alert("Sent!");
+    },
+    (err) => {
+      btn.value = "Send Email";
+      alert(JSON.stringify(err));
+    }
+  );
+});
