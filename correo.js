ea12a47911913251156964e8207022d0fefc11eb
Ronny Toledo <139065928+RonnyToledo@users.noreply.github.com>
2023-10-08 19:47:15 -0400

    Add files via upload

--- correo.js
+++ correo.js
@@ -3,18 +3,14 @@ const btn = document.getElementById("enviar-whatsapp");
 document.getElementById("form").addEventListener("submit", function (event) {
   event.preventDefault();
 
-  btn.value = "Sending...";
-
   const serviceID = "default_service";
   const templateID = "template_hgv6gkb";
 
   emailjs.sendForm(serviceID, templateID, this).then(
     () => {
-      btn.value = "Send Email";
-      alert("Sent!");
+      alert("Correo enviado");
     },
     (err) => {
-      btn.value = "Send Email";
       alert(JSON.stringify(err));
     }
   );
